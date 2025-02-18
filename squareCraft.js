9dbcd5dd471b510b6046956a6f3e543a038d27b2
fatin-webEfo <fatin@webefo.com>
2025-02-18 15:50:31 +0600

    feat: enhance dropdown styling with margin adjustments for improved layout

--- squareCraft.js
+++ squareCraft.js
@@ -283,8 +283,8 @@
         px
     </div>
     
-       <div  class="squareCraft-bg-color-2c2c2c  squareCraft-py-1px squareCraft-roudned-r-md">
-           <img id="squareCraftFontSizeDropdown" src="https://fatin-webefo.github.io/squareCraft-Plugin/public/arrow.svg" class="squareCraft-mr-1 squareCraft-ml-1 squareCraft-bg-color-2c2c2c squareCraft-rotate-180 squareCraft-px-1 squareCraft-mx-auto squareCraft-cursor-pointer" width="18px">
+       <div  class="squareCraft-bg-color-2c2c2c  squareCraft-roudned-r-md">
+           <img id="squareCraftFontSizeDropdown" src="https://fatin-webefo.github.io/squareCraft-Plugin/public/arrow.svg" class="squareCraft-mr-1 squareCraft-mb-1 squareCraft-ml-1 squareCraft-bg-color-2c2c2c squareCraft-rotate-180 squareCraft-px-1 squareCraft-mx-auto squareCraft-cursor-pointer" width="18px">
        </div>
     </div>
     
