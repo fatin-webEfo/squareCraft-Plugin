7f09dc38bcbd9fd70ddcfb260ae7d03f46d06553
fatin-webEfo <fatin@webefo.com>
2025-02-18 15:46:17 +0600

    feat: update stylesheet link to use external URL for improved accessibility

--- squareCraft.js
+++ squareCraft.js
@@ -33,7 +33,7 @@
   const link = document.createElement("link");
   link.rel = "stylesheet";  
   link.type = "text/css";
-  link.href = "./src/styles/parent.css";
+  link.href = "https://fatin-webefo.github.io/squareCraft-Plugin/src/styles/parent.css";
   document.head.appendChild(link);
 
   const fontSizes = [8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50];
