f350a74e248369f51f47a20b358d232f9b7a75a0
fatin-webEfo <fatin@webefo.com>
2025-02-22 12:44:35 +0600

    add

--- squareCraft.js
+++ squareCraft.js
@@ -259,7 +259,7 @@
         </div>
     
         <div class="squareCraft-mt-2 squareCraft-universal squareCraft-grid squareCraft-w-full squareCraft-grid-cols-12 squareCraft-gap-2 squareCraft-px-2">
-            <div id="squareCraft-font-family" class="squareCraft-flex squareCraft-col-span-8 squareCraft-cursor-pointer squareCraft-rounded-6px squareCraft-justify-between squareCraft-border squareCraft-border-solid squareCraft-border-585858 squareCraft-rounded-6px squareCraft-items-center squareCraft-h-full">
+            <div id="squareCraft-font-family" class="squareCraft-flex squareCraft-universal squareCraft-col-span-8 squareCraft-cursor-pointer squareCraft-rounded-6px squareCraft-justify-between squareCraft-border squareCraft-border-solid squareCraft-border-585858 squareCraft-rounded-6px squareCraft-items-center squareCraft-h-full">
                 <div class="squareCraft-bg-494949 squareCraft-w-full squareCraft-px-2 squareCraft-py-1px ">
                     <p class="squareCraft-text-sm squareCraft-font-light">Sf Pro sans</p>
                 </div>
