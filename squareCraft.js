90fe5c0ce41a0340dcba9cf46d8fdf15178dc6df
fatin-webEfo <fatin@webefo.com>
2025-02-22 12:50:37 +0600

    add

--- squareCraft.js
+++ squareCraft.js
@@ -261,9 +261,9 @@
         <div class="squareCraft-mt-2 squareCraft-universal squareCraft-grid squareCraft-w-full squareCraft-grid-cols-12 squareCraft-gap-2 squareCraft-px-2">
             <div id="squareCraft-font-family" class="squareCraft-flex squareCraft-universal squareCraft-col-span-8 squareCraft-cursor-pointer squareCraft-rounded-6px squareCraft-justify-between squareCraft-border squareCraft-border-solid squareCraft-border-585858 squareCraft-rounded-6px squareCraft-items-center squareCraft-h-full">
                 <div class="squareCraft-bg-494949 squareCraft-w-full squareCraft-universal squareCraft-px-2 squareCraft-py-1px ">
-                    <p class="squareCraft-text-sm squareCraft-universal squareCraft-font-light">Sf Pro sans</p>
+                    <p class="squareCraft-text-sm  squareCraft-font-light">Sf Pro sans</p>
                 </div>
-                <div class="squareCraft-bg-3f3f3f squareCraft-universal squareCraft-px-2" style="height: 27px; padding: 0 8px;">
+                <div class="squareCraft-bg-3f3f3f squareCraft-px-2" style="height: 27px; padding: 0 8px;">
                     <img class="squareCraft-h-full squareCraft-rotate-180" width="12px"
                         src="https://fatin-webefo.github.io/squareCraft-Plugin/public/arrow.svg" alt="">
     
