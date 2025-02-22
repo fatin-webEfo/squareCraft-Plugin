044df724b4609e0057f5aaf763f46c9c263daf7b
fatin-webEfo <fatin@webefo.com>
2025-02-22 11:34:37 +0600

    add

--- squareCraft.js
+++ squareCraft.js
@@ -184,7 +184,7 @@
     <div class="squareCraft-flex squareCraft-items-center squareCraft-justify-between">
         <img class="squareCraft-cursor-grabbing" src="https://i.ibb.co.com/pry1mVGD/Group-28-1.png" width="140px" />
         <div
-            class="squareCraft-flex squareCraft-items-center squareCraft-1px squareCraft-rounded-15px squareCraft-gap-2 squareCraft-bg-color-3d3d3d squareCraft-px-2">
+            class="squareCraft-flex squareCraft-items-center squareCraft-py-1px squareCraft-rounded-15px squareCraft-gap-2 squareCraft-bg-color-3d3d3d squareCraft-px-2">
             <p class="squareCraft-text-sm">Auto Save</p>
             <img src="https://i.ibb.co.com/B2NjHwSq/redo-rectangle.png" width="16px" />
         </div>
@@ -229,7 +229,7 @@
                 <p id="toggleText" class="squareCraft-text-sm">Enable</p>
             </div>
             <div id="resetButton" 
-            class="squareCraft-flex squareCraft-cursor-pointer squareCraft-items-center squareCraft-1px squareCraft-rounded-15px squareCraft-gap-2 squareCraft-bg-3f3f3f squareCraft-px-2">
+            class="squareCraft-flex squareCraft-cursor-pointer squareCraft-items-center squareCraft-py-1px squareCraft-rounded-15px squareCraft-gap-2 squareCraft-bg-3f3f3f squareCraft-px-2">
             <p class="squareCraft-text-sm">Reset</p>
             <img id="resetIcon" 
                 src="https://fatin-webefo.github.io/squareCraft-Plugin/public/reset.svg"
@@ -242,10 +242,10 @@
             <div
                 class="squareCraft-flex squareCraft-px-2 squareCraft-w-full squareCraft-items-center squareCraft-justify-between squareCraft-gap-2">
                 <div
-                    class="squareCraft-cursor-pointer squareCraft-bg-color-EF7C2F squareCraft-w-full squareCraft-font-light squareCraft-flex squareCraft-items-center squareCraft-text-sm squareCraft-1px squareCraft-rounded-6px squareCraft-text-color-white squareCraft-justify-center">
+                    class="squareCraft-cursor-pointer squareCraft-bg-color-EF7C2F squareCraft-w-full squareCraft-font-light squareCraft-flex squareCraft-items-center squareCraft-text-sm squareCraft-py-1px squareCraft-rounded-6px squareCraft-text-color-white squareCraft-justify-center">
                     Normal</div>
                 <div
-                    class="squareCraft-cursor-pointer squareCraft-bg-3f3f3f squareCraft-w-full squareCraft-text-color-white squareCraft-font-light squareCraft-flex squareCraft-text-sm squareCraft-1px squareCraft-rounded-6px squareCraft-items-center squareCraft-justify-center">
+                    class="squareCraft-cursor-pointer squareCraft-bg-3f3f3f squareCraft-w-full squareCraft-text-color-white squareCraft-font-light squareCraft-flex squareCraft-text-sm squareCraft-py-1px squareCraft-rounded-6px squareCraft-items-center squareCraft-justify-center">
                     Hover</div>
             </div>
             <div class="squareCraft-px-4">
@@ -260,7 +260,7 @@
     
         <div class="squareCraft-mt-2 squareCraft-grid squareCraft-w-full squareCraft-grid-cols-12 squareCraft-gap-2 squareCraft-px-2">
             <div id="squareCraft-font-family" class="squareCraft-flex squareCraft-col-span-8 squareCraft-cursor-pointer squareCraft-rounded-6px squareCraft-justify-between squareCraft-border squareCraft-border-solid squareCraft-border-585858 squareCraft-rounded-6px squareCraft-items-center squareCraft-h-full">
-                <div class="squareCraft-bg-494949 squareCraft-w-full squareCraft-px-2 squareCraft-1px ">
+                <div class="squareCraft-bg-494949 squareCraft-w-full squareCraft-px-2 squareCraft-py-1px ">
                     <p class="squareCraft-text-sm squareCraft-font-light">Sf Pro sans</p>
                 </div>
                 <div class="squareCraft-bg-3f3f3f squareCraft-px-2" style="height: 27px; padding: 0 8px;">
@@ -282,7 +282,7 @@
     
         <input type="text" id="squareCraftFontSizeInput" value="16" 
             class="squareCraft-font-size-input squareCraft-font-light squareCraft-text-sm squareCraft-text-color-white 
-            squareCraft-bg-transparent squareCraft-w-full  squareCraft-1px squareCraft-font-light">
+            squareCraft-bg-transparent squareCraft-w-full  squareCraft-py-1px squareCraft-font-light">
         
      
        <div class="squareCraft-flex squareCraft-items-center squareCraft-justify-center squareCraft-items-center">
@@ -299,7 +299,7 @@
             squareCraft-rounded-6px squareCraft-border squareCraft-border-585858 squareCraft-absolute 
             squareCraft-mt-1">
             ${fontSizes?.map(size => `
-                <div class="squareCraft-dropdown-item squareCraft-1px squareCraft-text-center  squareCraft-text-sm" 
+                <div class="squareCraft-dropdown-item squareCraft-py-1px squareCraft-text-center  squareCraft-text-sm" 
                     data-value="${size}">${size}</div>
             `).join('')}
         </div>
@@ -316,7 +316,7 @@
             
                 <div class="squareCraft-mt-2 squareCraft-grid squareCraft-px-2 squareCraft-w-full squareCraft-grid-cols-12 squareCraft-gap-2 ">
                     <div class="squareCraft-flex squareCraft-col-span-7 squareCraft-justify-between squareCraft-border squareCraft-border-solid squareCraft-border-585858 squareCraft-rounded-6px squareCraft-items-center squareCraft-h-full">
-                        <div class="squareCraft-bg-494949 squareCraft-px-2 squareCraft-w-full  squareCraft-1px ">
+                        <div class="squareCraft-bg-494949 squareCraft-px-2 squareCraft-w-full  squareCraft-py-1px ">
                             <p class="squareCraft-text-sm squareCraft-font-light">Regular</p>
                         </div>
                         <div class="squareCraft-bg-3f3f3f squareCraft-px-2" style="height: 27px; padding: 0 8px;">
@@ -374,7 +374,7 @@
               squareCraft-border-solid squareCraft-border-3d3d3d  squareCraft-rounded-6px 
               squareCraft-h-full">
               <input type="text" id="squareCraftLetterSpacingInput" value="15px" class="squareCraft-Letter-spacing-input squareCraft-font-light squareCraft-text-sm squareCraft-text-color-white 
-                squareCraft-bg-transparent squareCraft-w-full  squareCraft-1px squareCraft-font-light">
+                squareCraft-bg-transparent squareCraft-w-full  squareCraft-py-1px squareCraft-font-light">
               <div class="">
                 <img id="squareCraftLetterSpacingDropdown"
                   src="https://fatin-webefo.github.io/squareCraft-Plugin/public/line-spacing.svg"
@@ -385,7 +385,7 @@
               squareCraft-rounded-6px squareCraft-border squareCraft-border-585858 squareCraft-absolute 
               squareCraft-mt-1">
               ${LetterSpacing?.map(gap => `
-              <div class="squareCraft-dropdown-item squareCraft-1px squareCraft-text-center  squareCraft-text-sm"
+              <div class="squareCraft-dropdown-item squareCraft-py-1px squareCraft-text-center  squareCraft-text-sm"
                 data-value="${gap}">${gap}</div>
               `).join('')}
             </div>
@@ -527,7 +527,7 @@ async function fontfamilies() {
           const option = document.createElement("div");
           option.textContent = font.family;
           option.style.fontFamily = font.family;
-          option.classList.add("squareCraft-1px", "squareCraft-text-white", "squareCraft-text-sm", 
+          option.classList.add("squareCraft-py-1px", "squareCraft-text-white", "squareCraft-text-sm", 
               "squareCraft-cursor-pointer", "squareCraft-px-2");
 
           option.addEventListener("mouseover", () => option.classList.add("squareCraft-bg-494949"));
